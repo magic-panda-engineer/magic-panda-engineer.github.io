a5dd866af949a7895ae4ff88b3f4a916b8d0128e
KoKo Mexcelsa <qqkerk@hotmail.com>
2020-07-18 14:09:54 +0800

    Site updated: 2020-07-18 14:09:53

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/Hexo/hexo-404-and-letter-case.html');
+  precacheUrls.push('/ChatBot/dialogflow-in-AT-Tech.html');
 
   precacheUrls.push('/AI/yolo-v1-slides.html');
+
+  precacheUrls.push('/AI/Azure-Computer-Vision-Program-Sample.html');
 
   precacheUrls.push('/AI/Azure-Computer-Vision-Tutorial.html');
 
   precacheUrls.push('/database/Design-datebase-do-not-use-boolean.html');
-
-  precacheUrls.push('/Coding-Life/Ko-Ko-is-awarded-Microsoft-MVP.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
